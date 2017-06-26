a1b4150381fefd1c6996e7bf5089b47e1086cfc7
Simon Richardson <stickupkid@gmail.com>
2017-06-26 12:50:20 +0100

    Better architecture management

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,12 +1,60 @@
 import React from 'react';
+import Loader from 'react-loader';
 import Dropdown from './Dropdown.jsx';
+import Table from './Table.jsx';
 
 export default class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Food Hygiene</h1>
-        <Dropdown />
-      </div>);
-  }
+    constructor(props) {
+        super(props);
+
+         this.state = {
+            loaded: false,
+            authorities: [],
+            ratings: []
+         };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        // When we mount, that's when we want to get the authorities from the
+        // server. We make sure that the response is valid, if it's not then
+        // we also handle that.
+        const headers = new Headers();
+        headers.set('content-type', 'application/json');
+
+        const init = { headers };
+        fetch(`/query/authorities`, init)
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                throw new Error('Invalid response');
+            })
+            .then(res => this.setState(Object.assign(this.state, { loaded: true, authorities: res })))
+            .catch(err => this.setState(Object.assign(this.state, { loaded: true, authorities: [] })))
+            .then(res => {
+                // State when everything is loaded
+                if (this.state.authorities.length > 0) {
+                    const head = this.state.authorities[0];
+                    this.handleChange({ localId: head.local_id.toString() });
+                }
+            });
+    }
+
+    handleChange(event) {
+        console.log(event);
+    }
+
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1>Food Hygiene</h1>
+                <Loader loaded={this.state.loaded}>
+                    <Dropdown data={this.state.authorities} onChange={this.handleChange} />
+                    <Table data={this.state.ratings} />
+                </Loader>
+            </div>
+        );
+    }
 }
---
+++ ui/src/components/Table.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default class Table extends React.Component {
+    render() {
+        if (this.props.data.length < 1) {
+            return (<div></div>);
+        }
+
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rating</th>
+                        <th>Percentage</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.data.map((rating, i) =>
+                            <tr key={i}>
+                                <td>{rating.rating}</td>
+                                <td>{rating.percent}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        );
+    }
+}
